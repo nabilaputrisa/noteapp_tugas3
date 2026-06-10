6dcd6e42f093c70eab1e98cdfecb682e882aac91
nabilaputrisa <zearaa.seandita@gmail.com>
2026-06-10 14:43:37 +0700

    changeapp

--- frontend-nabs/script.js
+++ frontend-nabs/script.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://be-rest-saa-255520032221.asia-southeast1.run.app/api/notes';
+const API_URL = ' https://be-rest-255520032221.us-central1.run.app';
 
 let currentEditId = null;
 
